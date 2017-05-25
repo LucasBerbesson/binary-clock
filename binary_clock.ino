9589744a04a75ff8d7dbbf6e96392d16d18a1339
LucasBerbesson <lucasberbesson@gmail.com>
2017-05-25 20:59:40 +0200

    switch comments to english

--- binary_clock.ino
+++ binary_clock.ino
@@ -1,13 +1,8 @@
-/***********************************************************
-Description: Horloge binaire
-***********************************************************/
-
-// definir l'heure a l'allumage ici
-int second=0, minute=0, hour=0;
+int second=0, minute=0, hour=0; //start the time on 00:00:00
 
 
 int munit,hunit,valm=0,valh=0,ledstats,i;
-// Matrice de description des positions des LEDS
+// LEDS positions matrix
 int leds[4][4] = {
  {17,2,17,1},
  {17,5,4,3},
@@ -29,54 +24,51 @@ second=0;
 void loop() {
 
 static unsigned long lastTick = 0;
-// Cette variable va nous permettre de compter les secondes qui passent
-// dans la boucle qui suit
+// counting time here
 if (millis() - lastTick >= 1000) {
   lastTick = millis();
   second++;
 }
 
-// On avance d'une minute toutes les 60 secondes
-  if (second >= 60) {
+// Add 1 minute every 60 seconds
+if (second >= 60) {
   minute++;
-  second = 0; // et les secondes repartent a 0
+  second = 0; // reset seconds
 }
 
-// On avance d'une heure toutes les 60 minutes
+// Add 1 hour every 60 minutes
 if (minute >=60) {
   hour++;
-  minute = 0; // et les minutes repartent a 0
+  minute = 0; // reset minutes
 }
 
-// On avance d'une journee toutes les 24 heures
+// Add 1 day every 24 hour
 if (hour >=24) {
-  hour  = 0;
-  minute  = 0; // et les heures repartent a 0
+  hour  = 0; // reset hours
+  minute  = 0; // reset minutes
 }
 
-  // Le calcul qui suit permet de convertir l'heure en systeme binaire
-  munit = minute%10; // reste de la division des minutes par 10
-  hunit = hour%10; // reste de la division des heures par 10
-
-  //conversion des minutes. Chaque ligne gere une ligne de la quatrieme colonne.
+  // Folowing code is to convert time to binary system
+  munit = minute%10;
+  hunit = hour%10;
+  // Convert minutes. Each row handles a line of the fourth column
   if(munit == 1 || munit == 3 || munit == 5 || munit == 7 || munit == 9) {  digitalWrite(leds[3][3], HIGH);} else {  digitalWrite(leds[3][3],LOW);}
   if(munit == 2 || munit == 3 || munit == 6 || munit == 7) {digitalWrite(leds[2][3], HIGH);} else {digitalWrite(leds[2][3],LOW);}
   if(munit == 4 || munit == 5 || munit == 6 || munit == 7) {digitalWrite(leds[1][3], HIGH);} else {digitalWrite(leds[1][3],LOW);}
   if(munit == 8 || munit == 9) {digitalWrite(leds[0][3], HIGH);} else {digitalWrite(leds[0][3],LOW);}
 
-  //conversion des dizaines de minutes. Chaque ligne gere une ligne de la troisieme colonne.
+  // Convert minutes. Each row handles a line of the third column
   if((minute >= 10 && minute < 20) || (minute >= 30 && minute < 40) || (minute >= 50 && minute < 60))  {digitalWrite(leds[3][2], HIGH);} else {digitalWrite(leds[3][2],LOW);}
   if(minute >= 20 && minute < 40)  {digitalWrite(leds[2][2], HIGH);} else {digitalWrite(leds[2][2],LOW);}
   if(minute >= 40 && minute < 60) {digitalWrite(leds[1][2], HIGH);} else {digitalWrite(leds[1][2],LOW);}
 
-  //conversion des heures. Chaque ligne gere une ligne de la deuxieme colonne.
+  // Convert hours. Each row handles a line of the second column
   if(hunit == 1 || hunit == 3 || hunit == 5 || hunit == 7 || hunit == 9) {digitalWrite(leds[3][1], HIGH);} else {digitalWrite(leds[3][1],LOW);}
   if(hunit == 2 || hunit == 3 || hunit == 6 || hunit == 7) {digitalWrite(leds[2][1], HIGH);} else {digitalWrite(leds[2][1],LOW);}
   if(hunit == 4 || hunit == 5 || hunit == 6 || hunit == 7) {digitalWrite(leds[1][1], HIGH);} else {digitalWrite(leds[1][1],LOW);}
   if(hunit == 8 || hunit == 9) {digitalWrite(leds[0][1], HIGH);} else {digitalWrite(leds[0][1],LOW);}
 
-
-  //conversion des dizaines d'heures. Chaque ligne gere une ligne de la premiere colonne.
+  // Convert hours. Each row handles a line of the first column
   if(hour >= 10 && hour < 20)  {digitalWrite(leds[3][0], HIGH);} else {digitalWrite(leds[3][0],LOW);}
   if(hour >= 20 && hour < 24)  {digitalWrite(leds[2][0], HIGH);} else {digitalWrite(leds[2][0],LOW);}
 }
--- binary_clock_rtc_raw.ino
+++ binary_clock_rtc_raw.ino
@@ -1,5 +1,5 @@
 /*
-An open-source binary clock for Arduino. 
+An open-source binary clock for Arduino.
 Based on the code from by Rob Faludi (http://www.faludi.com)
 Code under (cc) by Daniel Spillere Andrade, www.danielandrade.net
 http://creativecommons.org/license/cc-gpl
@@ -15,18 +15,19 @@ Time  t;
 
 int second=0, minute=0, hour=0; //start the time on 00:00:00
 int munit,hunit,valm=0,valh=0,ledstats,i;
-int leds[4][4] = { 
+// LEDS positions matrix
+int leds[4][4] = {
  {17,1,17,0},
  {17,13,2,3},
  {10,9,7,4},
  {11,12,8,5}
 };
-void setup() { 
-  //set outputs 
+void setup() {
+  //set outputs
   for(int k=0;k<=13;k++) {
     pinMode(k, OUTPUT);
     digitalWrite(k, LOW);
-    
+
   }
   Serial.begin(9600);
   Serial.println("setup...");
@@ -52,7 +53,7 @@ void loop() {
   if(munit == 4 || munit == 5 || munit == 6 || munit == 7) {digitalWrite(leds[1][3], HIGH);} else {digitalWrite(leds[1][3],LOW);}
   if(munit == 8 || munit == 9) {digitalWrite(leds[0][3], HIGH);} else {digitalWrite(leds[0][3],LOW);}
 
-  //minutes 
+  //minutes
   if((minute >= 10 && minute < 20) || (minute >= 30 && minute < 40) || (minute >= 50 && minute < 60))  {digitalWrite(leds[3][2], HIGH);} else {digitalWrite(leds[3][2],LOW);}
   if(minute >= 20 && minute < 40)  {digitalWrite(leds[2][2], HIGH);} else {digitalWrite(leds[2][2],LOW);}
   if(minute >= 40 && minute < 60) {digitalWrite(leds[1][2], HIGH);} else {digitalWrite(leds[1][2],LOW);}
@@ -62,7 +63,6 @@ void loop() {
   if(hunit == 2 || hunit == 3 || hunit == 6 || hunit == 7) {digitalWrite(leds[2][1], HIGH);} else {digitalWrite(leds[2][1],LOW);}
   if(hunit == 4 || hunit == 5 || hunit == 6 || hunit == 7) {digitalWrite(leds[1][1], HIGH);} else {digitalWrite(leds[1][1],LOW);}
   if(hunit == 8 || hunit == 9) {digitalWrite(leds[0][1], HIGH);} else {digitalWrite(leds[0][1],LOW);}
-
 
   //hour
   if(hour >= 10 && hour < 20)  {digitalWrite(leds[3][0], HIGH);} else {digitalWrite(leds[3][0],LOW);}
