e507bbe2060940663637df31ddbf1398a6706158
LucasBerbesson <lucasberbesson@gmail.com>
2017-05-26 11:50:31 +0200

    use binary operators

--- binary_clock.ino
+++ binary_clock.ino
@@ -48,27 +48,28 @@ if (hour >=24) {
   minute  = 0; // reset minutes
 }
 
-  // Folowing code is to convert time to binary system
-  munit = minute%10;
+  munit = minute%10; //sets the variable munit and hunit for the unit digits
   hunit = hour%10;
-  // Convert minutes. Each row handles a line of the fourth column
-  if(munit == 1 || munit == 3 || munit == 5 || munit == 7 || munit == 9) {  digitalWrite(leds[3][3], HIGH);} else {  digitalWrite(leds[3][3],LOW);}
-  if(munit == 2 || munit == 3 || munit == 6 || munit == 7) {digitalWrite(leds[2][3], HIGH);} else {digitalWrite(leds[2][3],LOW);}
-  if(munit == 4 || munit == 5 || munit == 6 || munit == 7) {digitalWrite(leds[1][3], HIGH);} else {digitalWrite(leds[1][3],LOW);}
-  if(munit == 8 || munit == 9) {digitalWrite(leds[0][3], HIGH);} else {digitalWrite(leds[0][3],LOW);}
+  minuteTens = (int)(minute/10);
+  hourTens = (int)(hour/10);
+  //minutes units
+  if(munit & 1) {  digitalWrite(leds[3][3], HIGH);} else {  digitalWrite(leds[3][3],LOW);}
+  if(munit & 2) {digitalWrite(leds[2][3], HIGH);} else {digitalWrite(leds[2][3],LOW);}
+  if(munit & 4) {digitalWrite(leds[1][3], HIGH);} else {digitalWrite(leds[1][3],LOW);}
+  if(munit & 8) {digitalWrite(leds[0][3], HIGH);} else {digitalWrite(leds[0][3],LOW);}
 
-  // Convert minutes. Each row handles a line of the third column
-  if((minute >= 10 && minute < 20) || (minute >= 30 && minute < 40) || (minute >= 50 && minute < 60))  {digitalWrite(leds[3][2], HIGH);} else {digitalWrite(leds[3][2],LOW);}
-  if(minute >= 20 && minute < 40)  {digitalWrite(leds[2][2], HIGH);} else {digitalWrite(leds[2][2],LOW);}
-  if(minute >= 40 && minute < 60) {digitalWrite(leds[1][2], HIGH);} else {digitalWrite(leds[1][2],LOW);}
+  //minutes
+  if(minuteTens & 1)  {digitalWrite(leds[3][2], HIGH);} else {digitalWrite(leds[3][2],LOW);}
+  if(minuteTens & 2)  {digitalWrite(leds[2][2], HIGH);} else {digitalWrite(leds[2][2],LOW);}
+  if(minuteTens & 4) {digitalWrite(leds[1][2], HIGH);} else {digitalWrite(leds[1][2],LOW);}
 
-  // Convert hours. Each row handles a line of the second column
-  if(hunit == 1 || hunit == 3 || hunit == 5 || hunit == 7 || hunit == 9) {digitalWrite(leds[3][1], HIGH);} else {digitalWrite(leds[3][1],LOW);}
-  if(hunit == 2 || hunit == 3 || hunit == 6 || hunit == 7) {digitalWrite(leds[2][1], HIGH);} else {digitalWrite(leds[2][1],LOW);}
-  if(hunit == 4 || hunit == 5 || hunit == 6 || hunit == 7) {digitalWrite(leds[1][1], HIGH);} else {digitalWrite(leds[1][1],LOW);}
-  if(hunit == 8 || hunit == 9) {digitalWrite(leds[0][1], HIGH);} else {digitalWrite(leds[0][1],LOW);}
+  //hour units
+  if(hunit & 1) {digitalWrite(leds[3][1], HIGH);} else {digitalWrite(leds[3][1],LOW);}
+  if(hunit & 2) {digitalWrite(leds[2][1], HIGH);} else {digitalWrite(leds[2][1],LOW);}
+  if(hunit & 4) {digitalWrite(leds[1][1], HIGH);} else {digitalWrite(leds[1][1],LOW);}
+  if(hunit & 8) {digitalWrite(leds[0][1], HIGH);} else {digitalWrite(leds[0][1],LOW);}
 
-  // Convert hours. Each row handles a line of the first column
-  if(hour >= 10 && hour < 20)  {digitalWrite(leds[3][0], HIGH);} else {digitalWrite(leds[3][0],LOW);}
-  if(hour >= 20 && hour < 24)  {digitalWrite(leds[2][0], HIGH);} else {digitalWrite(leds[2][0],LOW);}
+  //hour
+  if(hourTens & 1)  {digitalWrite(leds[3][0], HIGH);} else {digitalWrite(leds[3][0],LOW);}
+  if(hourTens & 2)  {digitalWrite(leds[2][0], HIGH);} else {digitalWrite(leds[2][0],LOW);}
 }
--- binary_clock_rtc_raw.ino
+++ binary_clock_rtc_raw.ino
@@ -14,7 +14,7 @@ Time  t;
 
 
 int second=0, minute=0, hour=0; //start the time on 00:00:00
-int munit,hunit,valm=0,valh=0,ledstats,i;
+int munit,hunit,minuteTens,hourTens,valm=0,valh=0,ledstats,i;
 // LEDS positions matrix
 int leds[4][4] = {
  {17,1,17,0},
@@ -46,27 +46,28 @@ void loop() {
 
   munit = minute%10; //sets the variable munit and hunit for the unit digits
   hunit = hour%10;
-
+  minuteTens = (int)(minute/10);
+  hourTens = (int)(hour/10);
   //minutes units
-  if(munit == 1 || munit == 3 || munit == 5 || munit == 7 || munit == 9) {  digitalWrite(leds[3][3], HIGH);} else {  digitalWrite(leds[3][3],LOW);}
-  if(munit == 2 || munit == 3 || munit == 6 || munit == 7) {digitalWrite(leds[2][3], HIGH);} else {digitalWrite(leds[2][3],LOW);}
-  if(munit == 4 || munit == 5 || munit == 6 || munit == 7) {digitalWrite(leds[1][3], HIGH);} else {digitalWrite(leds[1][3],LOW);}
-  if(munit == 8 || munit == 9) {digitalWrite(leds[0][3], HIGH);} else {digitalWrite(leds[0][3],LOW);}
+  if(munit & 1) {  digitalWrite(leds[3][3], HIGH);} else {  digitalWrite(leds[3][3],LOW);}
+  if(munit & 2) {digitalWrite(leds[2][3], HIGH);} else {digitalWrite(leds[2][3],LOW);}
+  if(munit & 4) {digitalWrite(leds[1][3], HIGH);} else {digitalWrite(leds[1][3],LOW);}
+  if(munit & 8) {digitalWrite(leds[0][3], HIGH);} else {digitalWrite(leds[0][3],LOW);}
 
   //minutes
-  if((minute >= 10 && minute < 20) || (minute >= 30 && minute < 40) || (minute >= 50 && minute < 60))  {digitalWrite(leds[3][2], HIGH);} else {digitalWrite(leds[3][2],LOW);}
-  if(minute >= 20 && minute < 40)  {digitalWrite(leds[2][2], HIGH);} else {digitalWrite(leds[2][2],LOW);}
-  if(minute >= 40 && minute < 60) {digitalWrite(leds[1][2], HIGH);} else {digitalWrite(leds[1][2],LOW);}
+  if(minuteTens & 1)  {digitalWrite(leds[3][2], HIGH);} else {digitalWrite(leds[3][2],LOW);}
+  if(minuteTens & 2)  {digitalWrite(leds[2][2], HIGH);} else {digitalWrite(leds[2][2],LOW);}
+  if(minuteTens & 4) {digitalWrite(leds[1][2], HIGH);} else {digitalWrite(leds[1][2],LOW);}
 
   //hour units
-  if(hunit == 1 || hunit == 3 || hunit == 5 || hunit == 7 || hunit == 9) {digitalWrite(leds[3][1], HIGH);} else {digitalWrite(leds[3][1],LOW);}
-  if(hunit == 2 || hunit == 3 || hunit == 6 || hunit == 7) {digitalWrite(leds[2][1], HIGH);} else {digitalWrite(leds[2][1],LOW);}
-  if(hunit == 4 || hunit == 5 || hunit == 6 || hunit == 7) {digitalWrite(leds[1][1], HIGH);} else {digitalWrite(leds[1][1],LOW);}
-  if(hunit == 8 || hunit == 9) {digitalWrite(leds[0][1], HIGH);} else {digitalWrite(leds[0][1],LOW);}
+  if(hunit & 1) {digitalWrite(leds[3][1], HIGH);} else {digitalWrite(leds[3][1],LOW);}
+  if(hunit & 2) {digitalWrite(leds[2][1], HIGH);} else {digitalWrite(leds[2][1],LOW);}
+  if(hunit & 4) {digitalWrite(leds[1][1], HIGH);} else {digitalWrite(leds[1][1],LOW);}
+  if(hunit & 8) {digitalWrite(leds[0][1], HIGH);} else {digitalWrite(leds[0][1],LOW);}
 
   //hour
-  if(hour >= 10 && hour < 20)  {digitalWrite(leds[3][0], HIGH);} else {digitalWrite(leds[3][0],LOW);}
-  if(hour >= 20 && hour < 24)  {digitalWrite(leds[2][0], HIGH);} else {digitalWrite(leds[2][0],LOW);}
+  if(hourTens & 1)  {digitalWrite(leds[3][0], HIGH);} else {digitalWrite(leds[3][0],LOW);}
+  if(hourTens & 2)  {digitalWrite(leds[2][0], HIGH);} else {digitalWrite(leds[2][0],LOW);}
 
    valm = digitalRead(A1);    // add one minute when pressed
    if(valm== HIGH) {
